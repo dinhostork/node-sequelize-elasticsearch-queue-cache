df2d0e2f65ea7546d11c9baec7fdc1cccac451cc
Dinho Stork <dinhostork@live.com>
2023-12-18 18:21:19 -0300

    feat: add express route adapter

---
+++ src/main/adapters/express.adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { Controller } from '../../presentation/protocols';
+import { HttpRequest, HttpResponse } from '../../shared/http/http.protocol';
+
+export const adaptRoute = (controller: Controller) => {
+	return async (req: Request, res: Response) => {
+		const httpRequest: HttpRequest = {
+			headers: req.headers,
+			query: req.query,
+			body: req.body,
+			params: req.params,
+		};
+		const httpResponse: HttpResponse = await controller.handle(httpRequest);
+		res.status(httpResponse.statusCode).json(httpResponse.body);
+	};
+};
